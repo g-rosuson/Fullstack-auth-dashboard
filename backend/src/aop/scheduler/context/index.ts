ff31e67b15a2184ec30a6a70ec16285a9fedaed9
GHR <g.rosuson@gmail.com>
2026-07-15 12:52:20 +0200

    feat(backend): update scheduler and tests and link to FR's

--- backend/src/aop/scheduler/context/index.ts
+++ backend/src/aop/scheduler/context/index.ts
@@ -13,19 +13,18 @@ import type { Scheduler } from 'aop/scheduler';
 export class SchedulerContext {
     getAllJobs;
     schedule;
-    stop;
     delete;
     getNextAndPreviousRun;
     getNextRunFromPersistedSchedule;
+
     /**
      * Constructs a new SchedulerContext instance.
      * Initializes all domain methods with the provided scheduler instance.
      * @param scheduler The scheduler instance to wrap and provide to methods
      */
     constructor(scheduler: Scheduler) {
-        this.getAllJobs = scheduler.allJobs;
+        this.getAllJobs = scheduler.getAllJobs.bind(scheduler);
         this.schedule = scheduler.schedule.bind(scheduler);
-        this.stop = scheduler.stop.bind(scheduler);
         this.delete = scheduler.delete.bind(scheduler);
         this.getNextAndPreviousRun = scheduler.getNextAndPreviousRun.bind(scheduler);
         this.getNextRunFromPersistedSchedule = scheduler.getNextRunFromPersistedSchedule.bind(scheduler);
