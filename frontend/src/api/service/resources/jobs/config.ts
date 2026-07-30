3aac00232a40f0f79ba2bc31f5ae1eac77682bc4
GHR <g.rosuson@gmail.com>
2026-07-30 22:00:47 +0200

    feat(frontend): integrate job  api endpoints

--- frontend/src/api/service/resources/jobs/config.ts
+++ frontend/src/api/service/resources/jobs/config.ts
@@ -2,12 +2,16 @@ const domain = 'jobs/';
 
 const config = {
     path: {
+        stop: domain + 'stop/',
+        run: domain + 'run/',
         create: domain + 'create',
         update: domain + 'update/',
         delete: domain + 'delete/',
         getById: domain + 'get/',
         getAll: domain + 'get-all',
         streamAll: domain + 'stream-all',
+        changeScheduleStatus: domain + 'change-schedule-status/',
+        retrySchedule: domain + 'retry-schedule/',
     },
 };
 
